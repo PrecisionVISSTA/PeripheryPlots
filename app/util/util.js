6396da0b975dd7216ca771a2972ef0a7930c421e
Bryce Morrow <brycemorrow4564@gmail.com>
2019-05-06 13:37:21 -0400

    Added locks and bi-directional resize handles to the brushes

---
+++ app/util/util.js
@@ -0,0 +1,68 @@
+
+
+
+export function rotate (cx, cy, x, y, deg) {
+    /*
+    Rotatate the point (x,y) around the origin (cx,cy) 
+    by some angle (degrees)
+    */
+    let rad = (Math.PI / 180) * deg,
+        cos = Math.cos(rad),
+        sin = Math.sin(rad),
+        nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
+        ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+    return [nx, ny];
+} 
+        
+export function stringifyPoints (points) {
+    /*
+    Array of points [[x1,x2], ..., etc.] => string representation for svg 'points' property 
+    */
+    return points.reduce((acc,cur) => (`${acc}${cur[0]},${cur[1]} `), '')
+}
+        
+export function getEquilateralTriangleFromCentroid(centroid, s, o) {
+    /*
+    returns an equilateral triangle (defined as [[x1,y1], ..., etc.]) 
+    s is the distance from the geometric centroid to each of the vertices 
+    o is the orientation of the triangle (1 is UP, 0 is DOWN) 
+    */ 
+    let {x,y} = centroid; 
+    if (o == 0) {
+        //triangle points up if there was no change 
+        o = 1;
+    }
+    let so = s*o; 
+    let dv = so/2; 
+    let dh = so/2*Math.sqrt(3); 
+    return [[x,y+so],[x-dh,y-dv],[x+dh,y-dv]];
+}
+        
+export function tupdif(tup) {
+    return tup[1] - tup[0];
+}
+
+export function rangeeq(arr1, arr2) { 
+    return arr1[0] === arr2[0] && arr1[1] === arr2[1]; 
+}
+
+export function rangeIntersection(ar, br) {
+    /*
+    Determines if two intervals overlap. If the intervals overlap we return 
+    an array [oStart, oEnd]. If not, we return an empty array. 
+    */
+    let [a0, a1] = ar; 
+    let [b0, b1] = br; 
+    let intersection = b0 <= a1 ? [b0, a1] 
+                        : a0 <= b1 ? [a0, b1] 
+                        : []; 
+    let didIntersect = intersection.length == 2; 
+    return didIntersect, intersection; 
+}
+        
+export function range(stop) {
+    let arr = new Array(stop); 
+    for (let i = 0; i < stop; i++) { arr[i] = i; }
+    return arr; 
+}
+
